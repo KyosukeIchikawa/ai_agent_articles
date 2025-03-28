92fc79998161a6437979843c03c92c37da945318
KyosukeIchikawa <kyosuke.ichikawa@groove-x.com>
2025-03-28 13:04:59 +0900

    Fix links

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -131,12 +131,12 @@ export default function Background() {
         <div className="flex justify-between mt-10 pt-6 border-t border-gray-200 dark:border-gray-700">
           <div>
             <Link href="/ai_visual_arxiv/" className="text-blue-600 dark:text-blue-400 hover:underline">
-              ← トップページ
+              トップページ
             </Link>
           </div>
           <div>
             <Link href="/ai_visual_arxiv/method/" className="text-blue-600 dark:text-blue-400 hover:underline">
-              提案手法 →
+              次のセクション：3 Method
             </Link>
           </div>
         </div>
